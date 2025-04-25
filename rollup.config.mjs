59f4ab84e833a4ee59a8c39f89863bcf7f25cc02
Leonardo Ortiz <leo@monogram.io>
2025-04-25 18:17:11 -0300

    Upgrade to v3.0.0
    parent 76619c39b64efa53627bb10561b721bec97e2a18
    author Leonardo Ortiz <leo@monogram.io> 1745615831 -0300
    committer Chalo Salvador <chalo@monogram.io> 1748365978 +0200
    
    without test in name
    
    add amd build
    
    formatting from yjs, remove unused
    
    export Yjs from y-websocket, npm package name
    
    export awarenessProtocol
    
    latest version & latest changes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,5 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
   export default [{
     input: ['./src/y-websocket.js'],
     external: id => /^(lib0|yjs|y-protocols|ws|http)/.test(id),
@@ -8,5 +10,20 @@
       entryFileNames: '[name].cjs',
       chunkFileNames: '[name]-[hash].cjs'
     }]
-  }
-]
+  }, {
+    input: './src/y-websocket.js',
+    output: [{
+      dir: 'dist',
+      format: 'amd',
+      sourcemap: true,
+      entryFileNames: '[name].amd.js',
+      chunkFileNames: '[name]-[hash].amd.js',
+    }],
+    plugins: [
+      nodeResolve({
+        browser: true,
+        extensions: ['.js', '.ts'],
+        preferBuiltins: false
+      })
+    ]
+  }]
